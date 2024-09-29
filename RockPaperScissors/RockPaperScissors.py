c0d6eedcef8cfe83fdfd74de40251f2cfd4e7c8b
Ewan McCormick <ewantmccormick@gmail.com>
2024-09-28 21:00:21 -0400

    while loop

--- RockPaperScissors/RockPaperScissors.py
+++ RockPaperScissors/RockPaperScissors.py
@@ -1,4 +1,6 @@
 
+n = 0
+
 print("================== ROCK PAPER SCISSORS ==================")
 
 print("\nWelcome to Rock Paper Scissors!")
@@ -8,3 +10,9 @@ print("\nRules:")
 print("Scissors beats paper, paper beats rock, rock beats scissors.")
 
 roundNum = int(input("\nHow many rounds must a player win to win the match? "))
+
+print("Sounds great! Lets play...")
+
+while n < roundNum:
+    playerChoice = str(input("Choose Rock (R), Paper (P), or Scissors (S): "))
+    n = n + 1
