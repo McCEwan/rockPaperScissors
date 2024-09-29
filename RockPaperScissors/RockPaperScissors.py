94d35a86c640fcb62d7aec8a5b63ae61f2cc8e93
Ewan McCormick <ewantmccormick@gmail.com>
2024-09-28 20:22:17 -0400

    rulesAndIntro

---
+++ RockPaperScissors/RockPaperScissors.py
@@ -0,0 +1,10 @@
+
+print("================== ROCK PAPER SCISSORS ==================")
+
+print("\nWelcome to Rock Paper Scissors!")
+
+print("\nRules:")
+
+print("Scissors beats paper, paper beats rock, rock beats scissors.")
+
+roundNum = int(input("\nHow many rounds must a player win to win the match? "))
