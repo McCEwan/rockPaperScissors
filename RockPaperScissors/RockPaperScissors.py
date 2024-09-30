e013e541c35f3c8b59a7f011b08f1c9a67184e37
Ewan McCormick <ewantmccormick@gmail.com>
2024-09-30 00:55:28 -0400

    one more loop

--- RockPaperScissors/RockPaperScissors.py
+++ RockPaperScissors/RockPaperScissors.py
@@ -101,7 +101,96 @@ while roundNum > 1:
 while roundNum < 1:
 
     print("\n================== Please enter a whole number equal to or greater than 1 ==================")
-
+    
     roundNum = int(input("\nHow many rounds must a player win to win the match? "))
 
+    if roundNum > 1:
+        
+        print("Sounds great! Lets play...")
 
+    while pPointTotal < roundNum and cPointTotal < roundNum:
+            
+            compChoice = random.randrange(1,4)
+            
+            playerChoice = str(input("\nChoose Rock (R), Paper (P), or Scissors (S): "))
+            
+            if compChoice == 1:
+                
+                print("Computer: Rock ")
+                
+                if playerChoice == "r":
+                
+                    print("Tie!... Try again")
+                
+                elif playerChoice == "p":
+                
+                    pPointTotal = pPointTotal + 1
+                
+                    print("You win! Score:" ,pPointTotal, ":", cPointTotal)
+
+                elif playerChoice == "s":
+                
+                    cPointTotal = cPointTotal + 1
+
+                    print("I win! Score:", pPointTotal, ":" ,cPointTotal)
+
+                else:
+                
+                    print("Not sure what you mean. Try again...")
+                    
+        
+            elif compChoice == 2:
+            
+                print("Computer: Paper ")       
+            
+                if playerChoice == "r":
+                
+                    cPointTotal = cPointTotal + 1
+                
+                    print("I win! Score:" , pPointTotal, ":" ,cPointTotal)
+
+                elif playerChoice == "p":
+                
+                    print("Tie!... Try again")
+                
+                elif playerChoice == "s":
+
+                    pPointTotal = pPointTotal + 1
+                    
+                    print("You win! Score:" , pPointTotal, ":" ,cPointTotal)
+
+                else:
+                
+                    print("Not sure what you mean. Try again...")
+            
+            else:
+
+                print("Computer: Scissors ") 
+
+                if playerChoice == "r":
+                
+                    pPointTotal = pPointTotal + 1
+                    
+                    print("You win! Score:"  ,pPointTotal, ":"  ,cPointTotal)
+
+                elif playerChoice == "p":
+            
+                    cPointTotal = cPointTotal + 1
+                    
+                    print("I win! Score:" , pPointTotal, ":"  ,cPointTotal)
+                    
+                elif playerChoice == "s":
+                
+                    print("Tie!... Try again")
+
+                else:
+                
+                    print("Not sure what you mean. Try again...")
+
+if pPointTotal == roundNum:
+
+    print("You win the match! Do you want to play another match?")
+
+if cPointTotal == roundNum:
+
+    print("You lost the match! Do you want to play another match?")
